22ddeb675f0f9dca12537ebd26c0781bc89061e3
de-evg <d.e.min@yandex.ru>
2020-07-31 09:58:54 +0300

    настроил devServer

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.export = {
         filename: 'bundle.js',
         path: path.join(__dirname, 'publick')    
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        watchContentBase: true
+    }
 };
