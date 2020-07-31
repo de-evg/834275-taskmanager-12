e36a1c4639cc990970bc1fd0e4025bafaef48740
de-evg <d.e.min@yandex.ru>
2020-07-31 11:22:19 +0300

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.export = {
-    mode: 'development',
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'publick')    
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        watchContentBase: true
-    }
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'publick')
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true
+  }
 };
