c2cd05f1d5162b2e5f346359392ccec6d252a253
de-evg <d.e.min@yandex.ru>
2020-08-04 13:50:14 +0300

    добавил генерацию класса card-deadline по проверке сроков dealine

--- src/view/card.js
+++ src/view/card.js
@@ -1,3 +1,15 @@
+const isExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  let currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate = new Date(currentDate);
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
 const createCardTemplate = (task) => {
   const {color, description, dueDate} = task;
 
@@ -5,8 +17,12 @@ const createCardTemplate = (task) => {
     ? dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`})
     : ``;
 
+  const deadlineClassName = isExpired(dueDate)
+    ? `card--deadline`
+    : ``;
+
   return (
-    `<article class="card card--${color}">
+    `<article class="card card--${color} ${deadlineClassName}">
           <div class="card__form">
             <div class="card__inner">
               <div class="card__control">
