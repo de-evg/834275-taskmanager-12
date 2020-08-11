87b55967462a6a71b9920a8019f46b25672546ac
de-evg <d.e.min@yandex.ru>
2020-08-11 10:08:40 +0300

    вынес функции рендера в модуль с утилитами, описал компонент site-menu как класс, подключил в точку входа и выполнили рендер элемента

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {createSiteMenuTemplate} from "./view/site-menu.js";
+import SiteMenuView from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createBoardTemplate} from "./view/board.js";
 import {createSortTemplate} from "./view/sort.js";
@@ -11,6 +11,7 @@ import {createNoTasksMessageTemplate} from "./view/no-tasks-message.js";
 import {createStatisticTemplate} from "./view/statistic.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
+import {renderTemplate, renderElement, RenderPosition} from "./utils.js";
 
 const TASK_COUNT = 22;
 const TASK_COUNT_PER_STEP = 8;
@@ -21,29 +22,26 @@ const filters = generateFilter(tasks);
 const siteMainElement = document.querySelector(`.main`);
 const siteMainControlElement = siteMainElement.querySelector(`.main__control`);
 
-const render = (container, template) => {
-  container.insertAdjacentHTML(`beforeend`, template);
-};
 
-render(siteMainControlElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate(filters));
-render(siteMainElement, createBoardTemplate());
+renderElement(siteMainControlElement, new SiteMenuView(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createFilterTemplate(filters));
+renderTemplate(siteMainElement, createBoardTemplate());
 
 const boardElement = siteMainElement.querySelector(`.board`);
-render(boardElement, createSortTemplate());
-render(boardElement, createTasksListTemplate());
+renderTemplate(boardElement, createSortTemplate());
+renderTemplate(boardElement, createTasksListTemplate());
 
 const tasksListElement = boardElement.querySelector(`.board__tasks`);
-render(tasksListElement, createTaskEditTemplate(tasks[0]));
+renderTemplate(tasksListElement, createTaskEditTemplate(tasks[0]));
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  render(tasksListElement, createTaskTemplate(tasks[i]));
+  renderTemplate(tasksListElement, createTaskTemplate(tasks[i]));
 }
 
 
 if (tasks.length > TASK_COUNT_PER_STEP) {
   let renderedTaskCount = TASK_COUNT_PER_STEP;
 
-  render(boardElement, createLoadMoreBtnTemplate());
+  renderTemplate(boardElement, createLoadMoreBtnTemplate());
 
   const loadMoreBtnElement = boardElement.querySelector(`.load-more`);
 
@@ -51,13 +49,15 @@ if (tasks.length > TASK_COUNT_PER_STEP) {
     evt.preventDefault();
     tasks
       .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => render(tasksListElement, createTaskTemplate(task)));
+      .forEach((task) => renderTemplate(tasksListElement, createTaskTemplate(task)));
+
     renderedTaskCount += TASK_COUNT_PER_STEP;
+
     if (renderedTaskCount >= tasks.length) {
       loadMoreBtnElement.remove();
     }
   });
 }
-render(boardElement, createLoadingMessageTemplate());
-render(boardElement, createNoTasksMessageTemplate());
-render(siteMainElement, createStatisticTemplate());
+renderTemplate(boardElement, createLoadingMessageTemplate());
+renderTemplate(boardElement, createNoTasksMessageTemplate());
+renderTemplate(siteMainElement, createStatisticTemplate());
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,29 @@
+const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
 const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -39,4 +65,13 @@ const humanizeTaskDueDate = (dueDate) => {
   return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
 };
 
-export {getRandomInteger, isTaskExpired, isTaskExpiringToday, isTaskRepeating, humanizeTaskDueDate};
+export {
+  getRandomInteger,
+  isTaskExpired,
+  isTaskExpiringToday,
+  isTaskRepeating,
+  humanizeTaskDueDate,
+  renderElement,
+  renderTemplate,
+  createElement
+};
