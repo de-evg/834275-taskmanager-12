32ba575b022a9bbc2088f9660a35ed1503cc6fdf
de-evg <d.e.min@yandex.ru>
2020-08-11 14:22:15 +0300

    переименновал метод в render

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import StatisticView from "./view/statistic.js";
 
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
-import {renderTemplate, renderElement, RenderPosition} from "./utils.js";
+import {render, RenderPosition} from "./utils.js";
 
 const TASK_COUNT = 22;
 const TASK_COUNT_PER_STEP = 8;
@@ -24,37 +24,37 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const siteMenuComponent = new SiteMenuView();
-renderElement(siteHeaderElement, siteMenuComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, siteMenuComponent.getElement(), RenderPosition.BEFOREEND);
 const filterComponent = new FilterView(filters);
-renderElement(siteMainElement, filterComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, filterComponent.getElement(), RenderPosition.BEFOREEND);
 
 const boardComponent = new BoardView();
-renderElement(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
 
 const sortComponent = new SortView();
-renderElement(boardComponent.getElement(), sortComponent.getElement(), RenderPosition.AFTERBEGIN);
+render(boardComponent.getElement(), sortComponent.getElement(), RenderPosition.AFTERBEGIN);
 
 const taskListComponent = new TaskListView();
-renderElement(boardComponent.getElement(), taskListComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), taskListComponent.getElement(), RenderPosition.BEFOREEND);
 
 const taskEditComponent = new TaskEditView(tasks[0]);
-renderElement(taskListComponent.getElement(), taskEditComponent.getElement(), RenderPosition.BEFOREEND);
+render(taskListComponent.getElement(), taskEditComponent.getElement(), RenderPosition.BEFOREEND);
 
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderElement(taskListComponent.getElement(), new TaskView(tasks[i]).getElement(), RenderPosition.BEFOREEND);
+  render(taskListComponent.getElement(), new TaskView(tasks[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
 if (tasks.length > TASK_COUNT_PER_STEP) {
   let renderedTaskCount = TASK_COUNT_PER_STEP;
 
   const loadMoreBtnComponent = new LoadMoreBtnView();
-  renderElement(boardComponent.getElement(), loadMoreBtnComponent.getElement(), RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), loadMoreBtnComponent.getElement(), RenderPosition.BEFOREEND);
 
   loadMoreBtnComponent.getElement().addEventListener(`click`, (evt) => {
     evt.preventDefault();
     tasks
       .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderElement(taskListComponent.getElement(), new TaskView(task).getElement(), RenderPosition.BEFOREEND));
+      .forEach((task) => render(taskListComponent.getElement(), new TaskView(task).getElement(), RenderPosition.BEFOREEND));
 
     renderedTaskCount += TASK_COUNT_PER_STEP;
 
@@ -65,10 +65,10 @@ if (tasks.length > TASK_COUNT_PER_STEP) {
 }
 
 const loadingMessageComponent = new LoadingView();
-renderElement(boardComponent.getElement(), loadingMessageComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), loadingMessageComponent.getElement(), RenderPosition.BEFOREEND);
 
 const noTasksMessageComponent = new NoTaskView();
-renderElement(boardComponent.getElement(), noTasksMessageComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), noTasksMessageComponent.getElement(), RenderPosition.BEFOREEND);
 
 const statisticComponent = new StatisticView();
-renderElement(siteMainElement, statisticComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, statisticComponent.getElement(), RenderPosition.BEFOREEND);
